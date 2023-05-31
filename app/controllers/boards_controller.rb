f87d5ace564fd1ddc2b455e35e020e5e7a577645
Dan Brown <dbrown@occameducation.com>
2023-05-31 11:28:17 -0500

    queries fixed; views updated

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,23 +1,24 @@
 class BoardsController < ApplicationController
+   def sales
+      start_datem = (Time.current.beginning_of_month)
+      start_datey = (Time.current.beginning_of_year)
+      end_datem = (Time.current.end_of_month)
+      end_date = (Date.current)
+      date_ranges = [
+         ["@result_today", Date.current],
+         ["@result_month", start_datem..end_date],
+         ["@result_year", start_datey..end_date],
+         ["@budget_month", start_datem..end_datem]
+      ]
 
-def sales
-    start_datem = (Time.current.beginning_of_month)
-    start_datey = (Time.current.beginning_of_year)
-    end_datem = (Time.current.end_of_month)
-    end_date = (Date.current)
-    @result_today = Info.where(date: Date.current).select("COALESCE(SUM(ord), 0) AS ord_sum, COALESCE(SUM(val), 0) AS val_sum, COALESCE(SUM(bud_ord),
-     0) AS bud_ord_sum, COALESCE(SUM(bud_val), 0) AS bud_val_sum, COALESCE(SUM(py_ord), 0) AS py_ord_sum, COALESCE(SUM(py_val), 0) AS py_val_sum").group(:id).first
-    @result_month = Info.where(date: start_datem..end_date).select("COALESCE(SUM(ord), 0) AS ord_sum, COALESCE(SUM(val), 0) AS val_sum, COALESCE(SUM(bud_ord),
-     0) AS bud_ord_sum, COALESCE(SUM(bud_val), 0) AS bud_val_sum, COALESCE(SUM(py_ord), 0) AS py_ord_sum, COALESCE(SUM(py_val), 0) AS py_val_sum").group(:id).first
-    @result_year = Info.where(date: start_datey..end_date).select("COALESCE(SUM(ord), 0) AS ord_sum, COALESCE(SUM(val), 0) AS val_sum, COALESCE(SUM(bud_ord),
-     0) AS bud_ord_sum, COALESCE(SUM(bud_val), 0) AS bud_val_sum, COALESCE(SUM(py_ord), 0) AS py_ord_sum, COALESCE(SUM(py_val), 0) AS py_val_sum").group(:id).first
-    @budget_month = Info.where(date: start_datem..end_datem).select("COALESCE(SUM(ord), 0) AS ord_sum, COALESCE(SUM(val), 0) AS val_sum, COALESCE(SUM(bud_ord),
-     0) AS bud_ord_sum, COALESCE(SUM(bud_val), 0) AS bud_val_sum, COALESCE(SUM(py_ord), 0) AS py_ord_sum, COALESCE(SUM(py_val), 0) AS py_val_sum").group(:id).first
-    
-end
+      date_ranges.each do |var_name, range|
+         values = Info.where(date: range).pluck(Arel.sql("COALESCE(SUM(ord), 0), COALESCE(SUM(val), 0), COALESCE(SUM(bud_ord), 0), COALESCE(SUM(bud_val), 0), COALESCE(SUM(py_ord), 0), COALESCE(SUM(py_val), 0)")).first
+         hashed_result = %i[ord_sum val_sum bud_ord_sum bud_val_sum py_ord_sum py_val_sum].zip(values).to_h
+         instance_variable_set(var_name, hashed_result)
+      end
+   end
 
-def checksum
-   @check = result_today.val_sum - result_today.bud_val_sum 
-end
-
+   def checksum
+      @check = result_today.val_sum - result_today.bud_val_sum
+   end
 end
